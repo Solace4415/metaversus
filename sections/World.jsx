41eef1515b24ad891390893fa1038af54f36b2f4
Solace4415 <tijjtay@gmail.com>
2023-04-20 02:48:44 +0100

    changes made

--- sections/World.jsx
+++ sections/World.jsx
@@ -1,9 +1,10 @@
-'use client';
+"use client";
+import { motion } from "framer-motion";
+import { TypingText, NewFeatures, TitleText } from "../components";
+import styles from "../styles";
+import { newFeatures } from "../constants";
+import { staggerContainer, fadeIn, planetVariants } from "../utils/motion";
 
-const World = () => (
-  <section>
-    World section
-  </section>
-);
+const World = () => <section>World section</section>;
 
 export default World;
