80cb07b49691ab4e94b03c975dc7b36483925232
Solace4415 <tijjtay@gmail.com>
2023-04-22 11:20:55 +0100

    changes made

--- components/InsightCard.jsx
+++ components/InsightCard.jsx
@@ -1,6 +1,5 @@
 "use client";
 import { motion } from "framer-motion";
-import styles from "../styles";
 import { fadeIn } from "../utils/motion";
 
 const InsightCard = ({ index, imgUrl, title, subtitle }) => (
